2a97e3f1e3ce4a7e9e6e3f496372140409df3497
Salman Shakeel <salman.shakeel@clustox.com>
2023-08-27 20:38:34 +0500

    Changing camera impl

--- app/src/main/java/com/android/testapp/presentation/videorecorder/VideoRecorderActivity.kt
+++ app/src/main/java/com/android/testapp/presentation/videorecorder/VideoRecorderActivity.kt
@@ -11,12 +11,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import com.android.testapp.app.theme.TestAppTheme
 import com.android.testapp.presentation.components.CameraHeadsUpDisplay
 import com.android.testapp.presentation.videoplayer.VideoPlayerActivity
-import com.google.android.material.color.utilities.MaterialDynamicColors.onError
 import com.ujizin.camposer.CameraPreview
 import com.ujizin.camposer.state.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class VideoRecorderActivity : ComponentActivity() {
@@ -33,12 +37,17 @@ class VideoRecorderActivity : ComponentActivity() {
                     Box {
                         CameraPreview(
                             cameraState = cameraState,
-                            camSelector = camSelector,
-                        ) {
-
-                        }
+                            camSelector=camSelector,
+                            captureMode = CaptureMode.Video,
+                        )
                         CameraHeadsUpDisplay(this@VideoRecorderActivity, onCountDownFinish = {
-                            startRecording(cameraState)
+                            /*startRecording(cameraState)
+                            lifecycleScope.launch {
+                                withContext(Dispatchers.Default) {
+                                    waitingTime()
+                                }
+                                stopRecording(cameraState)
+                            }*/
                         })
                     }
                 }
@@ -46,18 +55,19 @@ class VideoRecorderActivity : ComponentActivity() {
         }
     }
 
-
     private fun startRecording(cameraState: CameraState) {
         cameraState.startRecording(
             file = File.createTempFile(
                 System.currentTimeMillis().toString(),
-                ".mp4",
-                externalCacheDir
+                ".mp4"
             ),
             onResult = { videoCaptureResult: VideoCaptureResult ->
                 when (videoCaptureResult) {
-                    is VideoCaptureResult.Error -> {}
+                    is VideoCaptureResult.Error -> {
+                        Log.d("HERERERER",videoCaptureResult.toString())
+                    }
                     is VideoCaptureResult.Success -> {
+                        Log.d("HERERERER","OnRecordResult")
                         startActivity(
                             Intent(
                                 this@VideoRecorderActivity,
@@ -69,11 +79,15 @@ class VideoRecorderActivity : ComponentActivity() {
                 }
             }
         )
-        //cameraState.stopRecording(file) { result -> /* ... */ }
+        Log.d("HERERERER","RecordingStarted")
     }
 
-    private fun stopRecording() {
+    suspend fun waitingTime(){
+        delay(5000)
+    }
 
+    private fun stopRecording(cameraState: CameraState) {
+        cameraState.stopRecording()
     }
 
     companion object {
